e1784b8b790a8af36e276aad9056ba60149b60cb
IIIAKZIII <alexis.kahne@free.fr>
2023-11-16 12:10:16 +0100

    Main Window

--- src/App.py
+++ src/App.py
@@ -7,11 +7,10 @@ class App:
 
     @staticmethod
     def new() -> None:
-        customtkinter.set_appearance_mode("dark")
-        customtkinter.set_default_color_theme("blue")
-        
         app = customtkinter.CTk()
+        
         app.geometry("1080x720")
         app.title("Projet NSI Terminale")
         app.resizable(False, False)
+        
         app.mainloop()
--- src/main.py
+++ src/main.py
@@ -1,6 +1,8 @@
 from App import *
 
 def main():
+    customtkinter.set_appearance_mode("dark")
+    customtkinter.set_default_color_theme("blue")
     App.new()
 
 if __name__ == '__main__':
