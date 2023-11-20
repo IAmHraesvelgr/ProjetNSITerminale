d7095de304b08477992e9c881ed964c4b8160c2b
IIIAKZIII <alexis.kahne@free.fr>
2023-11-20 14:10:58 +0100

    grid

--- src/App.py
+++ src/App.py
@@ -11,13 +11,14 @@ class App:
     def createGrid(app) -> None:
         for column in range(3):
             for row in range(3):
-                inputField = customtkinter.CTkEntry(app, width=3, height=3) 
-                inputField.grid()  
+                inputField = customtkinter.CTkEntry(app, width=100, height=100, font=customtkinter.CTkFont("Arial", 50, 'bold')) 
+                inputField.grid(row=row, column=column)  
 
     @staticmethod
     def new(title, width, height) -> None:
 
         app = customtkinter.CTk()
+        gridPanel = customtkinter.CTkFrame(master=app)
 
         App.title = title
         App.width = width
@@ -27,8 +28,10 @@ class App:
         app.title(App.title)
         app.resizable(False, False)
 
-        inputLabel = customtkinter.CTkLabel(app, font=customtkinter.CTkFont("Arial", 18, 'bold'), text="Entrez la grille de Sudoku à résoudre : ")
-        inputLabel.pack(pady=15)  
+        inputLabel = customtkinter.CTkLabel(app, font=customtkinter.CTkFont("Arial", 25, 'bold'), text="Entrez la grille de Sudoku à résoudre : ")  
+        
+        App.createGrid(gridPanel)
 
-        # App.createGrid(app)
+        inputLabel.pack(pady=15)
+        gridPanel.pack()
         app.mainloop()
