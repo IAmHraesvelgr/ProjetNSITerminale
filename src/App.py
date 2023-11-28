26504d90d7cb198df5671b3aeb05245b4d7136c1
IIIAKZIII <alexis.kahne@free.fr>
2023-11-28 12:22:44 +0100

    failed music

--- src/App.py
+++ src/App.py
@@ -1,11 +1,25 @@
 import os
+import pygame
 import tkinter
 import customtkinter
 
 class App:
 
     def __init__(self) -> None:
-        pass
+        pass    
+
+    @staticmethod
+    def playBackgroundMusic() -> None:
+        pygame.mixer.init()
+        pygame.mixer.music.load("../resources/backgroundmusic.mp3")
+        pygame.mixer.music.play(-1)
+    
+    @staticmethod
+    def stopBackgroundMusic() -> None:
+        try:
+            pygame.mixer.music.stop()
+        except:
+            return
 
     @staticmethod
     def checkGrid() -> None:
@@ -21,7 +35,7 @@ class App:
         
         for row in range(9):
             for column in range(9):
-                entry: customtkinter.CTkEntry = customtkinter.CTkEntry(panel, width=55, height=55, font=customtkinter.CTkFont("Helvetica", 50, 'bold'), border_width=2.5, corner_radius=0, justify="center")
+                entry: customtkinter.CTkEntry = customtkinter.CTkEntry(panel, width=55, height=55, font=App.font, border_width=2.5, corner_radius=0, justify="center")
             
                 pad_x: tuple = (0, 0)
                 pad_y: tuple = (0, 0)
@@ -38,21 +52,25 @@ class App:
         
 
     @staticmethod
-    def new(title: str, width: int, height: int) -> None:
-
+    def new(title: str, width: int, height: int) -> None:  
         app: customtkinter.CTk = customtkinter.CTk()
+        App.font: customtkinter.CTkFont = customtkinter.CTkFont("Helvetica", 25,'bold')
+        
         gridPanel: customtkinter.CTkFrame = customtkinter.CTkFrame(app, bg_color="transparent")
-        resolveGrid: customtkinter.CTkButton = customtkinter.CTkButton(app, text="Résoudre la Grille", corner_radius=32, width=300, height=50, font=customtkinter.CTkFont("Helvetica", 25,'bold'), command=App.resolveGrid)
+        resolveGrid: customtkinter.CTkButton = customtkinter.CTkButton(app, text="Résoudre la Grille", corner_radius=32, width=300, height=50, font=App.font, command=App.resolveGrid)
+        
+        playMusic: customtkinter.IntVar = customtkinter.IntVar()
+        musicButton: customtkinter.CTkCheckBox = customtkinter.CTkCheckBox(app, text="Musique", variable=playMusic, onvalue=1, offvalue=0, command=App.playBackgroundMusic() if playMusic == 1 else App.stopBackgroundMusic())
 
-        App.title = title
-        App.width = width
-        App.height = height
+        App.title: int = title
+        App.width: int = width
+        App.height: int = height
         
         app.minsize(App.width, App.height)
         app.title(App.title)
         app.resizable(False, False)
 
-        inputLabel: customtkinter.CTkLabel = customtkinter.CTkLabel(app, font=customtkinter.CTkFont("Helvetica", 25, 'bold'), text="Entrez la grille de Sudoku à résoudre : ")  
+        inputLabel: customtkinter.CTkLabel = customtkinter.CTkLabel(app, font=App.font, text="Entrez la grille de Sudoku à résoudre : ")  
     
         App.createGrid(gridPanel)
 
@@ -60,6 +78,7 @@ class App:
         gridPanel.pack(pady=35)
 
         resolveGrid.pack(pady=20, side=customtkinter.BOTTOM)
+        musicButton.pack(side=customtkinter.RIGHT)
 
         if os.name == "nt":
             import pywinstyles
