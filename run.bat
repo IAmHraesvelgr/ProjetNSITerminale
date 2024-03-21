2eaa26783d5aaacbcad35201820164eeb78194dd
Hræsvelgr <alexis.kahne@free.fr>
2024-03-21 11:23:13 +0100

    new installer files

--- run.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-pip install --upgrade pip
-pip install -r "./resources/requierments.txt"
-python "./src/main.py"
--- run.sh
+++
@@ -1,3 +0,0 @@
-python3 -m pip install --upgrade pip
-python3 -m pip install -r "./resources/requierments.txt"
-python3 "./src/main.py"
